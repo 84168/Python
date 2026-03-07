cdb7faa7c6557d844db60bfe4461451db5863929
tarun parmar <tarunparmar@TARUNs-MacBook-Air.local>
2026-03-07 13:43:51 +0530

    Initial commit

---
+++ hell.py
@@ -0,0 +1,11 @@
+str = "heismy\nthis is in next line "
+print(str[5:len(str)])
+print(str[-4:-1])
+print(str.endswith("line "))
+print(str.capitalize())
+print(str.replace("next" , "X"))
+print(str.find("this")) # STARTING INDEX OF 'THIS' first time jaha tha
+print(str.find("z"))
+str = input('enter name :')
+print(len(str))
+print(str.count("$"))
