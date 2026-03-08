fc1be0f58cc74b3b4348b270b6b72a7ed6cc14a8
tarun parmar <tarunparmar@TARUNs-MacBook-Air.local>
2026-03-08 22:19:23 +0530

    loops completed

---
+++ function.py
@@ -0,0 +1,10 @@
+# WE USE FUNCTION IN CODE TO REDUCE REDUNDENCY
+
+a= 5
+b = 10
+def sum(a ,b): # FUNCTION DEFNITION with parameters
+    print(a+b)
+
+sum(a,b) # FUNCTION CALLING WITH arguments
+
+print(print(print("hello")))
--- loops.py
+++ loops.py
@@ -1,4 +1,53 @@
-count = 1
-while(count <= 5):
-    print("hello", count)
-    count += 1
+# count = 1
+# while(count <= 5):
+#     print("hello", count)
+#     count += 1
+
+list = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# idx = 0
+# while(idx < len(list) ):
+#     print(list[idx])
+#     idx += 1
+
+# for val in list:
+#     print(val)
+# else:              # THIS ELSE WILL RUN AFTER COMPLETING EACH ITERATION OF LOOP IF BREAK THEN ELSE NOT EXECUTED
+#     print("end")
+
+# range(ele) => from 0 to ele - 1
+# seq = range(10)
+# for i in seq :
+#     print(i)
+
+# seq = range(1,5) # range(start, end)
+# for i in seq:
+#     print(i)
+
+# seq = range(1,5,2) # range(start, end, stepsize (can be negative) )
+# for i in seq:
+#     print(i)
+
+# num = int(input("ente number: "))
+# for i in range(1,11,2):
+#     print(i * num)
+
+# for i in range(5):
+#     pass # IF DONT WANT TO DO ANYTHING IN LOOP
+
+# print("some usefull work")
+
+# i = 1
+# sum =0
+# n = int(input("enter valu if N :"))
+# while(i<=n):
+#     sum += i
+#     i += 1
+# print(sum)
+
+# FACTORIAL 
+n = int(input("enter num"))
+fact = 1
+while(n>0):
+    fact *= n
+    n -= 1
+print(fact)
